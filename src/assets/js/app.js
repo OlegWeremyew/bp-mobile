fc4555fd4586a281d91c7ee6471ecc5b4423c21d
olegweremey <olegweremey1994@mail.ru>
2022-11-23 19:21:01 +0300

    added offers active item switcher and changed different styles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -42,33 +42,38 @@ const mainContent = getLanguage()
 const title = document.querySelector('.title')
 title.innerHTML = mainContent[`Unlimited Access<br>to All Features`];
 
-const content__text__1 = document.querySelector('.content__text--1')
-content__text__1.textContent = mainContent[`Unlimited documents`];
+const content__text1 = document.querySelector('.content__text1')
+content__text1.textContent = mainContent[`Unlimited documents`];
 
-const content__text__2 = document.querySelector('.content__text--2')
-content__text__2.textContent = mainContent[`Count mode`];
+const content__text2 = document.querySelector('.content__text2')
+content__text2.textContent = mainContent[`Count mode`];
 
-const content__text__3 = document.querySelector('.content__text--3')
-content__text__3.textContent = mainContent[`Text recognition (OCR)`];
+const content__text3 = document.querySelector('.content__text3')
+content__text3.textContent = mainContent[`Text recognition (OCR)`];
 
+const offer__title1 = document.querySelector('.offer__title1')
+offer__title1.textContent = mainContent[`Monthly`];
 
-const offer__title__1 = document.querySelector('.offer__title--1')
-offer__title__1.textContent = mainContent[`Monthly`];
+const offer__title2 = document.querySelector('.offer__title2')
+offer__title2.textContent = mainContent[`Annually`];
 
-const offer__title__2 = document.querySelector('.offer__title--2')
-offer__title__2.textContent = mainContent[`Annually`];
+const offer__price1 = document.querySelector('.offer__price1')
+offer__price1.innerHTML = mainContent[`<strong>{{price}}</strong><br>per month`];
 
-const offer__price__1 = document.querySelector('.offer__price--1')
-offer__price__1.innerHTML = mainContent[`<strong>{{price}}</strong><br>per month`];
+const offer__price2 = document.querySelector('.offer__price2')
+offer__price2.innerHTML = mainContent[`<strong>{{price}}</strong><br>per year`];
 
-const offer__price__2 = document.querySelector('.offer__price--2')
-offer__price__2.innerHTML = mainContent[`<strong>{{price}}</strong><br>per year`];
+const offer__status1 = document.querySelector('.offer__status1')
+offer__status1.textContent = mainContent[`3 DAYS FREE`];
 
-const offer__status__1 = document.querySelector('.offer__status--1')
-offer__status__1.textContent = mainContent[`3 DAYS FREE`];
+const offer__status2 = document.querySelector('.offer__status2')
+offer__status2.textContent = mainContent[`MOST POPULAR`];
 
-const offer__status__2 = document.querySelector('.offer__status--2')
-offer__status__2.textContent = mainContent[`MOST POPULAR`];
+const offer__month1 = document.querySelector('.offer__month1')
+offer__month1.textContent = mainContent["{{price}}/month"];
+
+const offer__month2 = document.querySelector('.offer__month2')
+offer__month2.textContent = mainContent["{{price}}/month"];
 
 const sending__btn = document.querySelector('.sending__btn')
 sending__btn.textContent = mainContent[`Continue`];
@@ -84,3 +89,25 @@ footer__link__term.textContent = mainContent[`Terms of Use`];
 
 const footer__link__privacy = document.querySelector('.footer__link--privacy')
 footer__link__privacy.textContent = mainContent[`Privacy Policy`];
+
+
+//active offers block
+const offer__item1 = document.querySelector('.offer__item1')
+const offer__item2 = document.querySelector('.offer__item2')
+const sending__link = document.querySelector('.sending__link')
+
+const switchActiveOfferItem = (event) => {
+  if (event.currentTarget.className.includes('offer__item1')) {
+    offer__item1.classList.add('active');
+    offer__item2.classList.remove('active');
+    sending__link.href = "https://apple.com/"
+    return;
+  }
+  offer__item2.classList.add('active');
+  offer__item1.classList.remove('active');
+  sending__link.href = "https://google.com/"
+  return;
+}
+
+offer__item1.addEventListener('click', switchActiveOfferItem)
+offer__item2.addEventListener('click', switchActiveOfferItem)
